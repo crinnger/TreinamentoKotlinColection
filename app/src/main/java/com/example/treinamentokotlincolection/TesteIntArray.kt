05da16e69bfed37299a488c54eae1a5c8ba1d650
Crinnger <crinnger@gmail.com>
2021-02-18 15:33:17 -0400

    Int Array

--- app/src/main/java/com/example/treinamentokotlincolection/TesteIntArray.kt
+++ app/src/main/java/com/example/treinamentokotlincolection/TesteIntArray.kt
@@ -2,4 +2,29 @@ package com.example.treinamentokotlincolection
 
 fun main(){
 
+    val values = IntArray(5)
+
+    values[0]=5
+    values[1]=2
+    values[2]=6
+    values[3]=3
+    values[4]=1
+
+    for(valor in values){
+        println(valor)
+    }
+    println("*******************************")
+    values.forEach { a ->
+        println(a)
+    }
+
+    println("*******************************")
+    for(index in values.indices){
+        println(values[index])
+    }
+    values.sort()
+    println("*******************************")
+    for(valor in values){
+        println(valor)
+    }
 }
